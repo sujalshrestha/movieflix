d41bd14330b776035741965d2f94c57376f59651
sujalshrestha <suj9763@gmail.com>
2025-11-24 09:31:50 +0545

    Initial Commit

---
+++ MovieFlix/MovieFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixApp.swift
+//  MovieFlix
+//
+//  Created by Sujal on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieFlixUITests/MovieFlixUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieFlixUITestsLaunchTests.swift
+//  MovieFlixUITests
+//
+//  Created by Sujal on 24/11/2025.
+//
+
+import XCTest
+
+final class MovieFlixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
